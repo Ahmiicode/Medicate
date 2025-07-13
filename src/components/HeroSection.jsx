dd6dfbc0d905f4cda6488454896abfc85d1b62e0
Ahmiicode <an1748452@gmail.com>
2025-07-13 09:48:12 +0500

    changing

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -18,10 +18,10 @@ const HeroSection = () => {
       <img src={logo} alt="Logo" className="md:h-43 h-18  w-auto" />
     </div>
       {/* Text Content */}
-      <div className="relative md:h-70 h-20 z-10  flex items-center justify-center text-white text-center px-4">
+      <div className="relative md:h-60 h-20 z-10  flex items-center justify-center text-white text-center px-4">
    
         <div className="md:mt-77 mt-66">
-          <h1 className="text-2xl md:text-6xl font-serif mb-4">
+          <h1 className="text-2xl md:text-5xl font-serif mb-4">
             Unlocking Tomorrow’s Possibilities Today
           </h1>
           <p className="text-lg md:text-2xl max-w-xl mx-auto">
